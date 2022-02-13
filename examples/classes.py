080f3d851cdcdce4dc7aad2778ef8f8ca34ff8e0
Donald J Ross <54287731+Fulgrim3@users.noreply.github.com>
2022-02-13 13:56:30 -0500

    enter

---
+++ examples/classes.py
@@ -0,0 +1,11 @@
+from tkinter import YView
+
+
+class Point(): 
+    def __inir__(self, input1, input2):
+        self.x = input1 
+        self.y = input2
+
+p = Point(2, 8)
+print(p.x)
+print(p.y)
